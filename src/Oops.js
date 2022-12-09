9fe296733e783fdc2407443b90a9799e93af39be
mehmeterk-computethis <34861665+mehmeterk-computethis@users.noreply.github.com>
2022-12-09 12:43:07 +0000

    Initial commit

---
+++ src/Oops.js
@@ -0,0 +1,13 @@
+import './Oops.css';
+export default function Oops(){
+
+  return (
+      <div className="Oops">
+      <div>
+        <p>Nothing to see here!</p>
+        <p>Come back later!</p>
+        </div>
+      <div></div>
+      </div>
+  );
+}
