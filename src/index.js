e9bdf9b655fea1dfb8c0f2f9a78eecdac398862b
Will McLean <willmcl@gmail.com>
2022-12-09 14:08:09 +0000

    Fresh install create-react-app

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
-//import Nav from './Nav';
-//import Footer from './Footer';
-import DeviceOri from './DeviceOri'
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DeviceOri/>
+    <App />
   </React.StrictMode>
 );
 
-//    <Nav/>
-//    <Footer/>
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
