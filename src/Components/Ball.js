3526852bdcd2f26d9a2f92a66ac47c42edc9e423
Will McLean <willmcl@gmail.com>
2022-12-14 16:37:08 +0000

    Float the objects away once clicked.

--- src/Components/Ball.js
+++ src/Components/Ball.js
@@ -1,4 +1,4 @@
-import React, {useMemo} from 'react';
+import React, {useState} from 'react';
 import {Instance} from "@react-three/drei";
 import {useSphere} from "@react-three/cannon";
 import * as THREE from "three";
@@ -6,6 +6,8 @@ import {useFrame} from "@react-three/fiber";
 
 const rfs = THREE.MathUtils.randFloatSpread
 function Ball({vec = new THREE.Vector3()}) {
+  const [ constrained, setConstrained ] = useState( true );
+
   // const sound = useMemo(() => new Audio('/bell.m4a'), [])
   //
   // const playAudio = (collision) => {
@@ -13,7 +15,7 @@ function Ball({vec = new THREE.Vector3()}) {
   //   if (collision.contact.impactVelocity > 3) {
   //     if(!sound.isPlaying){
   //       sound.currentTime = 0
-  //       sound.play((sound.volume = 0.2))
+  //       sound.play((sound.volume = 0.01 * collision.contact.impactVelocity))
   //     }
   //   }
   // }
@@ -28,19 +30,23 @@ function Ball({vec = new THREE.Vector3()}) {
   }))
 
   useFrame((state) => {
-    // for (let i = 0; i < props.count; i++) {
+    if(constrained) {
       // Get current whereabouts of the instanced sphere
       const matrix = ref.current.matrix
       // Normalize the position and multiply by a negative force.
       // This is enough to drive it towards the center-point.
       api.applyForce(vec.setFromMatrixPosition(matrix).normalize().multiplyScalar(-50).toArray(), [0, 0, 0])
-    // }
+    }
   })
 
   return (
     <Instance
     ref={ref}
-    castShadow receiveShadow />
+    castShadow receiveShadow
+    onClick={() => {
+      setConstrained(false)
+      api.applyLocalImpulse([0,-20,0], [0,0,0])
+    }}/>
   )
 }
 
